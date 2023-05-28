98c63163d41f3c70cde2ba80bd7349f8d434de9d
Langford Kwabena <55645613+langford-dev@users.noreply.github.com>
2023-05-28 15:52:13 +0000

    update: disable CSP warning in console
    add to index.ts:
    process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
